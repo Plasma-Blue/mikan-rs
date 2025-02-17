afd14a38e26d3230130335bd5093ec0f75e0a45b
Mutsumi <67679830+Plasma-Blue@users.noreply.github.com>
2025-02-18 00:14:04 +0800

    add: python api

---
+++ python/mikan/alias.py
@@ -0,0 +1,77 @@
+ALIAS_DICT = {
+
+    'accuracy': 'accuracy',
+    'acc': 'accuracy',
+    'Acc': 'accuracy',
+    'ACC': 'accuracy',
+    "Accuracy": "accuracy",
+    "RI": "accuracy",
+    "RandIndex": "accuracy",
+
+    'adjust_rand_score': 'adjust_rand_score',
+    'auc': 'auc',
+    'balanced_accuracy': 'balanced_accuracy',
+
+    'dice': 'dice',
+    'Dice': 'dice',
+    'dsc': 'dice',
+    'DSC': 'dice',
+
+    'f_score': 'f_score',
+
+    'jaccard_score': 'jaccard_score',
+    'kappa': 'kappa',
+
+    'mcc': 'mcc',
+    'MCC': 'mcc',
+    'MatthewsCorrelationCoefficient': 'mcc',
+
+    'nmcc': 'nmcc',
+    'NMCC': 'nmcc',
+    'nMCC': 'nmcc',
+    'MCC_normalized': "nmcc",
+
+    'amcc': 'amcc',
+    'AMCC': 'amcc',
+    'aMCC': 'amcc',
+    'MCC_absolute': 'amcc',
+
+    'precision': 'precision',
+    'senstivity': 'senstivity',
+    'specificity': 'specificity',
+    
+    'tn': 'tn',
+    'TN': 'tn',
+    'TrueNegative': 'tn',
+
+    'tp': 'tp',
+    'TP': 'tp',
+    'TruePositive': 'tp',
+
+    'fn': 'fn',
+    'FN': 'fn',
+    'FalseNegative': 'fn',
+
+    'fp': 'fp',
+    'FP': 'fp',
+    'FalsePositive': "fp",
+
+    'fnr': 'fnr',
+    'fpr': 'fpr',
+
+    'volume_similarity': 'volume_similarity',
+    'VolumetricSimilarity': 'volume_similarity',
+    'vs': "volume_similarity",
+    "VS": "volume_similarity",
+
+    'hausdorff_distance_95': 'hausdorff_distance_95',
+    'HD95': 'hausdorff_distance_95',
+    'hd95': 'hausdorff_distance_95',
+    'hausdorff_distance': 'hausdorff_distance',
+    'hd': 'hausdorff_distance',
+    'HD': 'hausdorff_distance',
+    'assd': 'assd',
+    'ASSD': 'assd',
+    'masd': 'masd',
+    'MASD': 'masd',
+}
